c2b04debb19990003b7e50154e20d8c05db6e69f
Abhishek Kumar <96721263+AbhishekKumar2002@users.noreply.github.com>
2025-06-08 01:13:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pickle
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)  # allow cross-origin requests from Next.js frontend
@@ -34,4 +35,5 @@ def predict():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
